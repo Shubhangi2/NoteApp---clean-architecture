bce058c8d7184e452f7bd4413d26fe56c13b2023
Shubhangi Jadhav <jshubhangi958@gmail.com>
2023-06-05 18:29:32 +0530

    dao database and repository created

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
@@ -1,8 +1,10 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.plcoding.cleanarchitecturenoteapp.ui.theme.*
 
+@Entity
 data class Note(
     val title: String,
     val content: String,
